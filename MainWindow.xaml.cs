dcc8b95537d9aaf7e2162218fb8bdc7b8e5167fa
fonerka <bbogosny@gmail.com>
2022-06-24 00:18:16 +0300

    startpoint(btnnotwrk)

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Armoury_Crate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseBtn_MouseEnter(object sender, MouseEventArgs e)
+        {
+            CloseBtn.Background = Brushes.Red;
+        }
+
+        //private void CloseBtn_MouseLeave(object sender, MouseEventArgs e)
+        //{
+        //    CloseBtn.Background = Brushes.Black;
+        //}
+    }
+}
